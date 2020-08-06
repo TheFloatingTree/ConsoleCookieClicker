2820424354e4a30d14930a40085a93513118b83d
thefloatingtree <thefloatingtree@gmail.com>
2020-08-06 10:32:33 -0600

    Updated code formatting

--- ConsoleCookieClicker/CookieClicker.cs
+++ ConsoleCookieClicker/CookieClicker.cs
@@ -32,6 +32,133 @@ namespace ConsoleCookieClicker
             Load();
         }
 
+        public void Run()
+        {
+            while (true)
+            {
+                DeltaTimer.Step();
+                InputManager.Update();
+                Update();
+                Draw();
+                ParticleManager.Run();
+                ConsoleGraphics.Draw();
+            }
+        }
+
+        public void Update()
+        {
+            if (InputManager.GetKey(ConsoleKey.Escape)) Exit();
+            if (InputManager.GetKey(ConsoleKey.Delete)) Reset();
+            if (InputManager.GetKey(ConsoleKey.Tab)) shouldAutoSave = !shouldAutoSave;
+
+            simulationTimer += DeltaTimer.Get();
+            autoSaveTimer += DeltaTimer.Get();
+            if (simulationTimer >= simulationInterval) SimulationTick();
+            if (autoSaveTimer >= autoSaveInterval) AutoSave();
+
+            cookie.Update();
+            wave.Update();
+            upgradeSelectList.Update();
+            upgradeList.Update(Console.WindowWidth - 1, 3);
+        }
+
+        public void Draw()
+        {
+            DrawUI();
+            wave.Draw();
+            cookie.Draw();
+            upgradeSelectList.Draw();
+            upgradeList.Draw();
+        }
+
+        public void SimulationTick()
+        {
+            simulationTimer -= simulationInterval;
+            data.Cookies += data.Upgrades.Where(item => item.Count > 0).Sum(item => item.CookiesPerTick());
+        }
+
+        private void BuildUI()
+        {
+            cookie = new Cookie();
+            wave = new Wave();
+
+            upgradeList = new UIList(Console.WindowWidth - 1, 3, data.Upgrades
+                .Select(upgrade => upgrade.DisplayText())
+                .ToList()
+                , ConsoleGraphics.TextAlign.Right);
+
+            upgradeSelectList = new UISelectList(1, 3, data.Upgrades
+                .Select(upgrade => upgrade.BuyText())
+                .ToList()
+                );
+
+            cookie.OnCookiePress += Cookie_OnCookiePress;
+            upgradeSelectList.OnItemPress += UpgradeSelectList_OnItemPress;
+        }
+
+        private void Cookie_OnCookiePress()
+        {
+            var cookieIncrease = data.Cookies / 100;
+            cookieIncrease = (cookieIncrease <= 1) ? 1 : cookieIncrease;
+            data.Cookies += cookieIncrease;
+        }
+
+        private void UpgradeSelectList_OnItemPress(string item)
+        {
+            UpgradeModel upgrade = data.Upgrades.Find(u => item == u.BuyText());
+
+            if (upgrade.Price() > data.Cookies) return;
+
+            data.Cookies -= upgrade.Price();
+            string previousText = upgrade.DisplayText();
+            upgrade.Count++;
+            upgradeList.ReplaceItem(previousText, upgrade.DisplayText());
+        }
+
+        private void Load()
+        {
+            try
+            {
+                data = CookieClickerRepository.GetMostRecentData();
+            }
+            catch
+            {
+                LoadDefaults();
+            }
+            BuildUI();
+        }
+
+        private void Reset()
+        {
+            CookieClickerRepository.DeleteAllData();
+            Load();
+        }
+
+        private void AutoSave()
+        {
+            if (!shouldAutoSave) return;
+            autoSaveTimer -= autoSaveInterval;
+            try
+            {
+                CookieClickerRepository.UpdateData(data);
+            }
+            catch 
+            {
+                shouldAutoSave = false;
+            }
+        }
+
+        public void Save()
+        {
+            CookieClickerRepository.SaveData(data);
+        }
+
+        public void Exit()
+        {
+            Save();
+            Environment.Exit(0);
+        }
+
         private void LoadDefaults()
         {
             data = new CookieClickerDataModel();
@@ -77,133 +204,6 @@ namespace ConsoleCookieClicker
                 BasePrice = 500000,
                 BaseCookiesPerTick = 2500
             });
-        }
-
-        private void Load()
-        {
-            try
-            {
-                data = CookieClickerRepository.GetMostRecentData();
-            }
-            catch
-            {
-                LoadDefaults();
-            }
-            BuildUI();
-        }
-
-        private void BuildUI()
-        {
-            // Build 
-            cookie = new Cookie();
-            wave = new Wave();
-            upgradeList = new UIList(Console.WindowWidth - 1, 3, data.Upgrades
-                .Select(upgrade => upgrade.DisplayText())
-                .ToList()
-                , ConsoleGraphics.TextAlign.Right);
-
-            upgradeSelectList = new UISelectList(1, 3, data.Upgrades
-                .Select(upgrade => upgrade.BuyText())
-                .ToList()
-                );
-
-            // Event Listeners
-            cookie.OnCookiePress += Cookie_OnCookiePress;
-            upgradeSelectList.OnItemPress += UpgradeSelectList_OnItemPress;
-        }
-
-        private void Reset()
-        {
-            CookieClickerRepository.DeleteAllData();
-            Load();
-        }
-
-        private void AutoSave()
-        {
-            if (!shouldAutoSave) return;
-            autoSaveTimer -= autoSaveInterval;
-            try
-            {
-                CookieClickerRepository.UpdateData(data);
-            }
-            catch 
-            {
-                shouldAutoSave = false;
-            }
-        }
-
-        public void Save()
-        {
-            CookieClickerRepository.SaveData(data);
-        }
-
-        private void UpgradeSelectList_OnItemPress(string item)
-        {
-            UpgradeModel upgrade = data.Upgrades.Find(u => item == u.BuyText());
-
-            if (upgrade.Price() > data.Cookies) return;
-
-            data.Cookies -= upgrade.Price();
-            string previousText = upgrade.DisplayText();
-            upgrade.Count++;
-            upgradeList.ReplaceItem(previousText, upgrade.DisplayText());
-        }
-
-        private void Cookie_OnCookiePress()
-        {
-            var cookieIncrease = data.Cookies / 100;
-            cookieIncrease = (cookieIncrease <= 1) ? 1 : cookieIncrease;
-            data.Cookies += cookieIncrease;
-        }
-
-        public void Run()
-        {
-            while (true)
-            {
-                DeltaTimer.Step();
-                InputManager.Update();
-                Update();
-                Draw();
-                ParticleManager.Run();
-                ConsoleGraphics.Draw();
-            }
-        }
-
-        public void SimulationTick()
-        {
-            simulationTimer -= simulationInterval;
-            data.Cookies += data.Upgrades.Where(item => item.Count > 0).Sum(item => item.CookiesPerTick());
-        }
-
-        public void Update()
-        {
-            if (InputManager.GetKey(ConsoleKey.Escape)) Exit();
-            if (InputManager.GetKey(ConsoleKey.Delete)) Reset();
-            if (InputManager.GetKey(ConsoleKey.Tab)) shouldAutoSave = !shouldAutoSave;
-
-            simulationTimer += DeltaTimer.Get();
-            autoSaveTimer += DeltaTimer.Get();
-            if (simulationTimer >= simulationInterval) SimulationTick();
-            if (autoSaveTimer >= autoSaveInterval) AutoSave();
-            cookie.Update();
-            wave.Update();
-            upgradeSelectList.Update();
-            upgradeList.Update(Console.WindowWidth - 1, 3);
-        }
-
-        public void Exit()
-        {
-            Save();
-            Environment.Exit(0);
-        }
-
-        public void Draw()
-        {
-            DrawUI();
-            wave.Draw();
-            cookie.Draw();
-            upgradeSelectList.Draw();
-            upgradeList.Draw();
         }
 
         public void DrawUI()
